bae0b23f5dff201e340e4a711ef0ea94beefb073
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-11 22:40:03 +0100

    restarting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-// import animals, { useAnimals } from "./data";
+import animals, { useAnimals } from "./data";
+
+console.log(animals);
+
+const [cat, dog] = animals;
 
 // //Destructuring Arrays
 // // console.log(animals);
@@ -17,44 +21,40 @@
 // // console.log(food);
 
 // CHALLENGE: uncomment the code below and see the car stats rendered
-import React from "react";
-import ReactDOM from "react-dom";
-import cars from "./practice";
 
-const [honda, tesla] = cars;
+// const [honda, tesla] = cars;
 
-const {
-  speedStats: { topSpeed: hondaTopSpeed }
-} = honda;
-const {
-  speedStats: { topSpeed: teslaTopSpeed }
-} = tesla;
+// const {
+//   speedStats: { topSpeed: hondaTopSpeed }
+// } = honda;
+// const {
+//   speedStats: { topSpeed: teslaTopSpeed }
+// } = tesla;
 
-const {
-  coloursByPopularity: [hondaTopColour]
-} = honda;
-const {
-  coloursByPopularity: [teslaTopColour]
-} = tesla;
+// const {
+//   coloursByPopularity: [hondaTopColour]
+// } = honda;
+// const {
+//   coloursByPopularity: [teslaTopColour]
+// } = tesla;
 
-
-ReactDOM.render(
-  <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top Speed</th>
-      <th>Top Colour</th>
-    </tr>
-    <tr>
-      <td>{tesla.model}</td>
-      <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td>
-    </tr>
-    <tr>
-      <td>{honda.model}</td>
-      <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
-    </tr>
-  </table>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//       <th>Top Colour</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
